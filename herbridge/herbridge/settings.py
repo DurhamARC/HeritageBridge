85d78cb73cd41d08bdcaf6161386a99eff87e0a1
Jordan <21129425+ItIsJordan@users.noreply.github.com>
2025-08-04 13:37:35 +0100

    Update empty variables in settings.py
    Adds blank variables for arches username, password and application client ID to settings.py which are used to manage arches access

--- herbridge/herbridge/settings.py
+++ herbridge/herbridge/settings.py
@@ -144,7 +144,12 @@ ADMIN_EMAIL = ''
 # setup default password and auth token for frontend app
 FRONTEND_AUTH_PASSWORD = 'eamena2018'
 FRONTEND_AUTH_TOKEN = 'cb9c4a76-ae93-46d8-a153-7e8525a9c2c2'
-EAMENA_TARGET = 'http://eamena-hb.legiongis.com'
+EAMENA_TARGET = ''
+
+# Arches Login information
+ARCHES_USERNAME = ''
+ARCHES_PASSWORD = ''
+ARCHES_CLIENT_ID = ''
 
 # names of fixtures that are loaded with db setup --loaddata
 FIXTURES_TO_LOAD = (
